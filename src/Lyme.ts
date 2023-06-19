74a6fa58b71e6e84d0787bdf4df3115429176ac9
chlorocodes <chlorocodestv@gmail.com>
2023-06-19 11:27:54 -0700

    ok

--- src/Lyme.ts
+++ src/Lyme.ts
@@ -157,6 +157,10 @@ export class Lyme {
   }
 
   private onBotMention(message: Message) {
+    if (message.content.includes('!debug')) {
+      console.log(message)
+    }
+
     this.handleDiscussionWithBot(message)
   }
 
@@ -170,6 +174,25 @@ export class Lyme {
 
   private async handleDiscussionWithBot(message: Message) {
     if (message.author.username === 'o_kayy') {
+      if (message.channel.id !== this.channelId) {
+        message.reply(
+          'O-kay, you are only allowed to interact with me in the #lyme channel. Additionally, every message you send to me must begin with "Please" and must end with "Thank you". An example: "@Lyme please tell me how to be a better person, thank you'
+        )
+        return
+      }
+    } else if (
+      !message.content.toLowerCase().startsWith('please') &&
+      !message.content.toLowerCase().startsWith('<@1110372412534571059> please')
+    ) {
+      message.reply('O-kay, you must begin every message with "Please"')
+      return
+    } else if (
+      !message.content.toLowerCase().endsWith('thanks') &&
+      !message.content.toLowerCase().endsWith('thank you')
+    ) {
+      message.reply(
+        'O-kay, you must end every message with "Thanks" or "Thank you'
+      )
       return
     }
 
